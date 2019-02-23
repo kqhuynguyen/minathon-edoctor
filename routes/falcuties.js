92d2b1c91f244978ab57b1587862ba161fc712a1
tadevi <1611695@hcmut.edu.vn>
2019-02-23 22:24:13 +0700

    Add search falcuty

--- routes/falcuties.js
+++ routes/falcuties.js
@@ -10,15 +10,13 @@ router.prefix('/diseases')
 
 //tim theo trieu chung
 router.get('/', async (ctx, next) => {
-  const symptom=ctx.query.symptom;
-  const regex=new RegExp(symptom, 'i');
+  const chuyenkhoa=ctx.query.name;
+  const regex=new RegExp(chuyenkhoa, 'i');
   let falcuties=null;
   let falcutyID=null;
-    falcuties=await Disease.find({symptom:regex})
-                  .select('falcutyID')
-    falcutyID=await falcuties.map(obj=>obj.falcutyID);
-
-
+  falcuties=await Falcuty.find({})
+                .select('id');
+  falcutyID=await falcuties.map(obj=>obj.id);
 
     const doctors=await Doctor
             .find({falcuty:{$elemMatch:{$in:falcutyID} }})
